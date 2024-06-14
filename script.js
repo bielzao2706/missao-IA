2c3d971f7d3490739aff20abd25dd46ccacea399
Gabriel Santos de Oliveira <108476373+bielzao2706@users.noreply.github.com>
2024-06-14 11:33:38 -0300

    edit

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado= document.querySelector(".caixa-resultado");
+const textoResultado= document.querySelector(".texto-resultado");
+
+const=perguntas [
+    
+    enunciado:"Perguntas 1",
+    alternativas:
+    "Alternativas 1",
+    "Alternativas 2"
+]
+},
+{
+enunciado: "Perguntas 2",
+alternativas: [
+    "Alternativas 1",
+    "Alternativas 2"
+]
+}
